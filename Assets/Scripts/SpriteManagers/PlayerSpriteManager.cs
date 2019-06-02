2009571b5be212d0f20869859c6bf1f0c3ae1e7e
Vin <vin.tansiri@gmail.com>
2019-06-03 00:53:08 +1000

    Sprite Layering Update

---
+++ Assets/Scripts/SpriteManagers/PlayerSpriteManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpriteManager : MonoBehaviour
+{
+    public SpriteRenderer rend;
+    public BoxCollider2D playerBox;
+    public float yPos;
+    public GameObject[] props;
+    public List<float> propPositions = new List<float>();
+    public List<SpriteRenderer> propSprites = new List<SpriteRenderer>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerBox = GameObject.FindGameObjectWithTag("Feet").GetComponent<BoxCollider2D>();
+        rend = GetComponentInParent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        BoxCollider2D feet = GameObject.FindGameObjectWithTag("Feet").GetComponent<BoxCollider2D>();
+        yPos = feet.bounds.center.y;
+    }
+
+    Color ChangeAlpha(Color color)
+    {
+        if (color.a > 0.5f)
+        {
+            color.a -= 0.5f * Time.deltaTime;
+        }
+
+        else if (color.a < 0.5f)
+        {
+            color.a -= 0.5f * Time.deltaTime;
+        }
+        return color;
+    }
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        PropSpriteManager prop = other.GetComponent<PropSpriteManager>();
+        if (prop)
+        {
+            Color color = prop.rend.color;
+
+            if (yPos < prop.yPos)
+            {
+                rend.sortingOrder = prop.rend.sortingOrder + 1;
+                prop.state = AlphaState.AlphaUp;
+            }
+
+            else if (yPos >= prop.yPos)
+            {
+                rend.sortingOrder = prop.rend.sortingOrder - 1;
+                prop.state = AlphaState.AlphaDown;
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        PropSpriteManager prop = other.GetComponent<PropSpriteManager>();
+        if (prop)
+        {
+            if (prop.state != AlphaState.AlphaUp)
+            {
+                prop.state = AlphaState.AlphaUp;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/SpriteManagers/PropSpriteManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropSpriteManager : MonoBehaviour
+{
+    public AlphaState state;
+    public Color color;
+    public SpriteRenderer rend;
+    public Collider2D propCollider;
+    public float yPos;
+    void Start()
+    {
+        color = GetComponent<SpriteRenderer>().color;
+        rend = GetComponent<SpriteRenderer>();
+        propCollider = transform.Find("Collider").GetComponent<BoxCollider2D>();
+        yPos = propCollider.bounds.center.y;
+    }
+
+    void Update()
+    {
+        switch (state)
+        {
+            case AlphaState.AlphaUp:
+                if (color.a < 1f)
+                {
+                    color.a += 2f * Time.deltaTime;
+                    GetComponent<SpriteRenderer>().color = color;
+                }
+                break;
+            case AlphaState.AlphaDown:
+                if (color.a > 0.5f)
+                {
+                    color.a -= 2f * Time.deltaTime;
+                    GetComponent<SpriteRenderer>().color = color;
+                }
+                break;
+            default:
+                if (color.a > 0.5f)
+                {
+                    color.a -= 2f * Time.deltaTime;
+                    GetComponent<SpriteRenderer>().color = color;
+                }
+                break;
+        }
+    }
+}
+public enum AlphaState { AlphaUp, AlphaDown }
